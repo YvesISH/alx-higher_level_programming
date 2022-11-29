0da5917473926075eff6e2833f91682a66da7e40
YvesISH <ivanmutware999@gmail.com>
2022-11-29 06:07:33 -0800

    If condition.

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -8,4 +8,4 @@ def fizzbuzz():
         elif number % 5 == 0:
             print("Buzz ", end="")
         else:
-            print("{}".format(number), end="")
+            print("{} ".format(number), end="")
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#print all numbers from 0 to 98
+# Print all numbers from 0 to 98 in decimal and in hexadecimal
 
 for n in range(0, 99):
     print("{} = {}".format(n, hex(n)))
